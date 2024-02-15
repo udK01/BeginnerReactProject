5746ac264106766869aed8daa98d713c3dda7814
udK01 <dominik.muranyi@gmail.com>
2024-02-15 18:51:11 +0000

    Conditional Rendering In React

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import UserGreeting from "./components/UserGreeting";
 import Student from "./components/Student";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -9,17 +10,21 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Food /> */}
+      <UserGreeting isLoggedIn={true} username="Business Monkey" />
       <Card />
-      {/* <Button /> */}
-      <Student name="Spongebob" age={30} isStudent={true} />
-      <Student name="Patrick" age={42} isStudent={false} />
-      <Student name="Squidward" age={50} isStudent={false} />
-      <Student name="Sandy" age={27} isStudent={true} />
-      <Student name="Larry" />
       <Footer />
     </>
   );
 }
 
 export default App;
+
+{
+  /* <Food />
+<Button />
+<Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name="Larry" /> */
+}
---
+++ src/components/UserGreeting.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+export default function UserGreeting(props) {
+  const welcomeMessage = (
+    <h2 className="welcome-message">Welcome {props.username}</h2>
+  );
+  const loginPrompt = (
+    <h2 className="login-prompt">Please Login To Continue!</h2>
+  );
+
+  return props.isLoggedIn ? welcomeMessage : loginPrompt;
+}
+UserGreeting.proptypes = {
+  isLoggedIn: PropTypes.bool,
+  username: PropTypes.string,
+};
+UserGreeting.defaultProps = {
+  isLoggedIn: false,
+  username: "Guest",
+};
