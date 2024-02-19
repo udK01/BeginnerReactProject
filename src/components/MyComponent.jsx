0d1ade1624dcf9a99e562c6ad51f725f29e1c1b3
udK01 <dominik.muranyi@gmail.com>
2024-02-19 14:38:27 +0000

    Emptied Component

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,46 +1,7 @@
 import { useEffect, useState, useRef } from "react";
-import Button from "./Button";
 
 export default function MyComponent() {
-  const inputRef1 = useRef(null);
-  const inputRef2 = useRef(null);
-  const inputRef3 = useRef(null);
-
-  useEffect(() => {
-    console.log("COMPONENT RENDERED");
-  });
-
-  function handleClick1() {
-    inputRef1.current.focus();
-    inputRef1.current.style.backgroundColor = "yellow";
-    inputRef2.current.style.backgroundColor = "";
-    inputRef3.current.style.backgroundColor = "";
-  }
-
-  function handleClick2() {
-    inputRef2.current.focus();
-    inputRef1.current.style.backgroundColor = "";
-    inputRef2.current.style.backgroundColor = "yellow";
-    inputRef3.current.style.backgroundColor = "";
-  }
-
-  function handleClick3() {
-    inputRef3.current.focus();
-    inputRef1.current.style.backgroundColor = "";
-    inputRef2.current.style.backgroundColor = "";
-    inputRef3.current.style.backgroundColor = "yellow";
-  }
-
-  return (
-    <>
-      <Button btnText={"Click Me 1!"} function={handleClick1} />
-      <input ref={inputRef1} />
-      <Button btnText={"Click Me 2!"} function={handleClick2} />
-      <input ref={inputRef2} />
-      <Button btnText={"Click Me 3!"} function={handleClick3} />
-      <input ref={inputRef3} />
-    </>
-  );
+  return <></>;
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -323,5 +284,51 @@ export default function MyComponent() {
 
 // <p>Window Width {width}px</p>
 // <p>Window Height {height}px</p>
+
+/////////////////////////////////////////////////////////////////////////
+
+// Example 7.
+
+// import Button from "./Button";
+
+// const inputRef1 = useRef(null);
+// const inputRef2 = useRef(null);
+// const inputRef3 = useRef(null);
+
+// useEffect(() => {
+//   console.log("COMPONENT RENDERED");
+// });
+
+// function handleClick1() {
+//   inputRef1.current.focus();
+//   inputRef1.current.style.backgroundColor = "yellow";
+//   inputRef2.current.style.backgroundColor = "";
+//   inputRef3.current.style.backgroundColor = "";
+// }
+
+// function handleClick2() {
+//   inputRef2.current.focus();
+//   inputRef1.current.style.backgroundColor = "";
+//   inputRef2.current.style.backgroundColor = "yellow";
+//   inputRef3.current.style.backgroundColor = "";
+// }
+
+// function handleClick3() {
+//   inputRef3.current.focus();
+//   inputRef1.current.style.backgroundColor = "";
+//   inputRef2.current.style.backgroundColor = "";
+//   inputRef3.current.style.backgroundColor = "yellow";
+// }
+
+// <Button btnText={"Click Me 1!"} function={handleClick1} />
+// <input ref={inputRef1} />
+// <Button btnText={"Click Me 2!"} function={handleClick2} />
+// <input ref={inputRef2} />
+// <Button btnText={"Click Me 3!"} function={handleClick3} />
+// <input ref={inputRef3} />
+
+/////////////////////////////////////////////////////////////////////////
+
+// Example 8.
 
 /////////////////////////////////////////////////////////////////////////
